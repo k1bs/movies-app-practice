33596397f69a9b5521f0783472635612b7a1bc61
Alex Kibler <alexkibler@me.com>
2017-11-16 11:24:41 -0500

    create login and register components

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  handleInputChange (e) {
+    const name = e.target.name
+    const val = e.target.value
+    this.setState({
+      [name]: val
+    })
+  }
+  render () {
+    return (
+      <div>
+        <form onSubmit={(e) => this.props.handleLoginSubmit(e, this.state)}>
+          <input type='text' name='username' value={this.state.username} placeholder='Username' onChange={this.handleInputChange} />
+          <input type='password' name='password' value={this.state.password} placeholder='Password' onChange={this.handleInputChange} />
+          <inut type='submit' value='Login' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      email: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  handleInputChange (e) {
+    const name = e.target.name
+    const val = e.target.value
+    this.setState({
+      [name]: val
+    })
+  }
+  render () {
+    return (
+      <div>
+        <form onSubmit={(e) => this.props.handleRegisterSubmit(e, this.state)}>
+          <input type='text' name='username' value={this.state.username} placeholder='Username' onChange={this.handleInputChange} />
+          <input type='password' name='password' value={this.state.password} placeholder='Password' onChange={this.handleInputChange} />
+          <input type='text' name='email' value={this.state.email} placeholder='Email Address' onChange={this.handleInputChange} />
+          <input type='submit' value='Register' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Register
