7ae61d86643a1410d5d97cce5d6e43ead1245e32
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-16 10:11:14 -0500

    "Fixed indentations and other linter error messages"

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './components/Home'
@@ -15,17 +17,17 @@ class App extends Component {
   }
 
   render () {
-   return (
-     <Router>
-       <div className='App'>
-         <Header />
-         <div className='container'>
-           <Route exact path='/' component={Home} />
-         </div>
-         <Footer />
-       </div>
-     </Router>
-   )
+    return (
+      <Router>
+        <div className='App'>
+          <Header />
+          <div className='container'>
+            <Route exact path='/' component={Home} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    )
   }
 }
 
