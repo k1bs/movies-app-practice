1789a1a7fbde421609c2e32f3b618f888077aaba
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-16 11:03:56 -0500

    "Uncomment code in server to test routes on postman...Created findById method"

--- models/Movie.js
+++ models/Movie.js
@@ -6,4 +6,10 @@ Movie.findAll = () => {
   return db.query(`SELECT * FROM movies`)
 }
 
+Movie.findById = (id) => {
+  return db.oneOrNone(
+   `SELECT * FROM movies
+   WHERE id = $1`, [id])
+}
+
 module.exports = Movie
