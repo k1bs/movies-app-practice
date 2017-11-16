abf45c29575bc9d25e951b289fac4c13e9456867
Alex Kibler <alexkibler@me.com>
2017-11-16 10:40:10 -0500

    fix migration

--- db/migrations/migration-11162017.sql
+++ db/migrations/migration-11162017.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(255) UNIQUE NOT NULL,
   password_digest TEXT NOT NULL,
   email VARCHAR(255)
-)
+);
 
 CREATE TABLE IF NOT EXISTS movies (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   description TEXT,
   genre VARCHAR(255),
-  user_id INT REFERENCES user(id)
-)
+  user_id INT REFERENCES users(id)
+);
