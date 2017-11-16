8294a201db880b24d6e7cb0e7c3060aed6f78a9c
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-16 11:43:26 -0500

    "Finished controller method for delete...working on route"

--- controllers/movies-controller.js
+++ controllers/movies-controller.js
@@ -35,18 +35,28 @@ movieController.create = (req, res, next) => {
     })
   }).catch(next)
 }
- movieController.update = (req, res, next) => {
-   Movie.update({
-     title: req.body.title,
-     description: req.body.description,
-     genre: req.body.genre,
-   }, req.params.id)
-   .then(movie => {
-     res.json({
-       message: 'Movie updated successfully',
-       data: { movie }
-     })
-   }).catch(next)
- }
- 
+
+movieController.update = (req, res, next) => {
+  Movie.update({
+    title: req.body.title,
+    description: req.body.description,
+    genre: req.body.genre
+  }, req.params.id)
+  .then(movie => {
+    res.json({
+      message: 'Movie updated successfully',
+      data: { movie }
+    })
+  }).catch(next)
+}
+
+movieController.delete = (req, res, next) => {
+  Movie.destroy(req.params.id)
+  .then(() => {
+    res.json({
+      message: 'Movie removed successfully!'
+    })
+  }).catch(next)
+}
+
 module.exports = movieController
