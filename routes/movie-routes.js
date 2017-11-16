25670edc8db5fd6633ff901704860dcadf00db7a
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-16 11:45:16 -0500

    "finished all neccessary routes"

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -3,11 +3,11 @@ const movieController = require('../controllers/movies-controller')
 const express = require('express')
 const movieRoutes = express.Router()
 
-
-movieRoutes.get('/', movieController.index);
-movieRoutes.get('/:id', movieController.show)
+movieRoutes.get('/', movieController.index)
 movieRoutes.post('/', movieController.create)
 
+movieRoutes.get('/:id', movieController.show)
 movieRoutes.put('/:id', movieController.update)
+movieRoutes.delete('/:id', movieController.delete)
 
 module.exports = movieRoutes
