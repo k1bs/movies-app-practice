23940432093ce8979918dbd78c372c4960b4a35b
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-16 10:51:26 -0500

    "Created findAll method in model"

--- models/Movie.js
+++ models/Movie.js
@@ -6,11 +6,4 @@ Movie.findAll = () => {
   return db.query(`SELCET * FROM movies`)
 }
 
-Movie.findById = (id) => {
-  return db.oneOrNone(
-   `SELECT * FROM movies
-   WHERE id = $1`, [id])
-}
-
-
 module.exports = Movie
