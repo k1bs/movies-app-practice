03c217cb3ef9b7ecaf5cb95f6e0981ce483ff883
Olivia Stoick <oliviastoick@gmail.com>
2017-11-16 12:04:26 -0500

    updated movielist

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import Movie from './Movie'
+import MovieForm from './MovieForm'
 
 class MovieList extends Component {
   constructor(props){
     super(props)
     this.state = {
       movies:null,
-      dataLoaded:false
+      dataLoaded:false,
+      auth: props.auth,
+      currentlyEditing:null
     }
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.setEditing = this.setEditing.bind(this)
   }
 
   componentDidMount() {
@@ -23,6 +28,12 @@ class MovieList extends Component {
           dataLoaded:true,
         })
       }).catch(err => console.log(err))
+  }
+
+  setEditing(id) {
+    this.setState({
+      currentlyEditing: id,
+    })
   }
 
   handleFormSubmit(method, e, data, id) {
@@ -45,7 +56,9 @@ class MovieList extends Component {
   renderMovieList() {
     if (this.state.dataLoaded) {
       return this.state.movies.map(movie => {
-        return <Movie key={movie.id} movie={movie} />
+        if (movie.id === this.state.currentlyEditing)
+          return <MovieForm movie={movie} handleFormSubmit={this.handleFormSubmit} isAdd={false} key={movie.id} />
+        else return <Movie key={movie.id} movie={movie} auth={this.state.auth} setEditing={this.setEditing} />
       })
     } else return <p>Loading...</p>
   }
@@ -53,6 +66,9 @@ class MovieList extends Component {
   render() {
     return (
       <div className="movielist">
+        {this.state.auth
+          ? <MovieForm isAdd={true} handleFormSubmit={this.handleFormSubmit} />
+          : ''}
         {this.renderMovieList()}
       </div>
       )
