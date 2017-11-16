56e8c37d1e35084d65a63ed463dae3e9c7744d39
Olivia Stoick <oliviastoick@gmail.com>
2017-11-16 11:44:03 -0500

    completed handleformsubmit

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
 class MovieList extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state = {
       movies:null,
       dataLoaded:false
