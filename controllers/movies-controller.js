18c6770e2f1f19386da01eb5f3dd9a73bcdd4ec4
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-16 10:54:32 -0500

    "Created movie.index method in controller"

---
+++ controllers/movies-controller.js
@@ -0,0 +1,13 @@
+const Movie = require('../models/Movie')
+
+const movieController = {}
+
+movieController.index = (req, res, next) => {
+  Movie.findAll()
+  .then(movies => {
+    res.json({
+      message: 'ok',
+      data: {movies}
+    })
+  }).catch(next)
+}
